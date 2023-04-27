79aff9d39f238fa625ca5bfc696f3a928c1495d2
Florian Dejean <florian.dejean@epfl.ch>
2023-04-27 15:00:34 +0200

    added graph

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,20 @@
 from flask import render_template, jsonify,request
 from app.main import bp
+from datetime import datetime, timedelta
+import time
+import random
+
+ip="168.20.13.1"
+startTime= time.time()
 
 @bp.route('/')
 def index():
-    ip="102"
+    # Make connection to ESP here
+
+    #Dummy data
+    connected = True
+  
+
     return render_template('index.html', ip=ip)
 
 @bp.route('/run-python-function', methods=['POST'])
@@ -19,4 +30,19 @@ def my_python_function(slider):
     
     print("hello from web")
     print(slider)
+    ip="aaa"
     print("")
+
+@bp.route('/get-graph-data', methods=['POST'])
+def get_graph_data():
+    x_sent = []
+    x_received = []
+    y_sent = []
+    y_received = []
+    for i in range(10):
+        x_sent.append(time.time()-startTime)
+        y_sent.append(random.randint(0, 10))
+        x_received.append(time.time()-startTime)
+        y_received.append(random.randint(0, 10))
+  
+    return jsonify(x_sent=x_sent, y_sent=y_sent, x_received=x_received,y_received=y_received)
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,7 +4,7 @@ function callPythonFunction(sliderValue) {
     type: "POST",
     data: { value: sliderValue },
     success: function(response) {
-      //console.log(response);
+      console.log(response);
     },
     error: function(error) {
       console.log(error);
@@ -15,4 +15,41 @@ function callPythonFunction(sliderValue) {
 document.getElementById("mySlider").addEventListener("input", function() {
   var sliderValue = this.value;
   callPythonFunction(sliderValue);
+});
+
+$(document).ready(function() {
+  
+  var layout = {
+    width: 500,
+    height: 300,
+    xaxis: {
+      tickmode: 'linear',
+      dtick: 0.25,
+      tickformat: '.2f',
+      title: 'Time (s)'
+    },
+    yaxis: {
+      title: 'Number of Packets'
+    }
+  };
+  var data = [{ x: [], y: [], type: 'bar', name: 'Sent' },
+              { x: [], y: [], type: 'bar', name: 'Received' }];
+  Plotly.newPlot('graph', data, layout);
+
+  function updateGraph() {
+    $.ajax({
+      url: '/get-graph-data',
+      type: 'POST',
+      success: function(response) {
+        var x_sent = response.x_sent;
+        var y_sent = response.y_sent;
+        var x_received = response.x_received;
+        var y_received = response.y_received;
+
+        Plotly.update('graph', { x: [x_sent, x_received], y: [y_sent, y_received] });
+      }
+    });
+  }
+
+  setInterval(updateGraph, 1000);
 });
