4a2e03b231efd5d4f751ca03dd398926361e1b92
fdejean <73421792+fdejean@users.noreply.github.com>
2023-04-28 09:10:32 +0200

    Two way communication

--- laptopSide/app/esp32Connection.py
+++ laptopSide/app/esp32Connection.py
@@ -3,17 +3,31 @@ import time
 import struct
 
 class ESP32Connection:
-    def __init__(self, host, port):
+    def __init__(self, host, send_port,recv_port):
         print("Establishing Connection")
         self.host = host
-        self.port = port
-        self.client = None
+        self.send_port = send_port
+        self.recv_port=recv_port
+        self.send_socket  = None
+        self.recv_socket = None
         self.connect()
 
     def connect(self):
         try:
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client.connect((self.host, self.port))
+            self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.send_socket.connect((self.host, self.send_port))
+            print("Send established successfully")
+           
+            # create a socket object for receiving data from the client
+            recv_socket = socket.socket()
+            recv_socket.bind(('0.0.0.0', 8090 ))  # bind to a local address and port
+            recv_socket.listen(0)  # start listening for incoming connections
+           
+
+            # wait for a client to connect
+            self.recv_socket, client_address = recv_socket.accept()
+            print('Recv established successfully by', client_address)
+
             print("Connection established successfully")
             return
         except Exception as e:
@@ -22,6 +36,16 @@ class ESP32Connection:
             time.sleep(1)
             self.connect()
 
+    def listen(self):
+
+
+        # Receive data from the client socket
+        data = self.recv_socket.recv(1024)
+
+        # Print the received data
+        print(f"Received data: {data}")
+
+    
     def send_angles(self, angle1, angle2, angle3):
         # Encode the angles as floats and pack them into a binary format
         packed_angles = struct.pack('fff', angle1, angle2, angle3)
--- laptopSide/app/run.py
+++ laptopSide/app/run.py
@@ -2,11 +2,13 @@ import esp32Connection as esp
 
 
  # IP address and port number of the ESP32
-host = '192.168.210.79'
-port = 8888
+host = '192.168.28.79'
+recv_port = 8090
+send_port = 8091
 
-esp = esp.ESP32Connection(host,port)
-esp.send_angles(20.4,10.2,10)
+esp = esp.ESP32Connection(host,send_port,recv_port)
+esp.listen()
+
 
 
 
