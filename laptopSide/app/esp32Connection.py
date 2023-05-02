c70d9bd064fd0e80b6646398ce813ae4b44e42f7
FacerOfGod <93382197+FacerOfGod@users.noreply.github.com>
2023-05-02 21:13:14 +0200

    Update esp32Connection.py
    Works but when disconnected, can't reconnect probablely because of method _is_socket_alive_  or line 76 (waiting to recieve something but nothing comes)

--- laptopSide/app/esp32Connection.py
+++ laptopSide/app/esp32Connection.py
@@ -1,113 +1,96 @@
 import socket
 import time
 import struct
+import threading
+import math
 
 class ESP32Connection:
-    def __init__(self, host, send_port,recv_port):
+    def __init__(self, send_port, recv_port):
         print("Establishing Connection")
-        self.host = host
+        self.host = None
+        self.connected = False
         self.send_port = send_port
-        self.recv_port=recv_port
-        self.send_socket  = None
+        self.recv_port = recv_port
+        self.send_socket = None
         self.recv_socket = None
-        self.connect()
+        self.recv_stat = []
+        self.send_stat = []
+        self.obstacle = {}
+        self.time = time.time()
 
-    def connect(self):
+                
+    def _connect(self):
+        print(" Connecting...")
         try:
-            self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.send_socket.connect((self.host, self.send_port))
-            print("Send established successfully")
-           
             # create a socket object for receiving data from the client
             recv_socket = socket.socket()
-            recv_socket.bind(('0.0.0.0', 8090 ))  # bind to a local address and port
-            recv_socket.listen(0)  # start listening for incoming connections
-           
+            recv_socket.bind(('0.0.0.0', self.recv_port))  # bind to a local address and port
+            recv_socket.listen()  # start listening for incoming connections
 
             # wait for a client to connect
             self.recv_socket, client_address = recv_socket.accept()
+
+
             print('Recv established successfully by', client_address)
+            self.host = client_address[0]
 
+            self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.send_socket.connect((self.host, self.send_port))
             print("Connection established successfully")
-            return
+            
+            self.connected = True
+
         except Exception as e:
             print("Connection error :", e)
-            print(" Reconnecting ...")
+            print("Reconnecting ...")
+            self.connected = False
             time.sleep(1)
-            self.connect()
-
-    def listen(self):
 
 
+
+############ COMUNICATION METHOD #############
+
+    def _listen(self):
         # Receive data from the client socket
-        data = self.recv_socket.recv(1024)
+        data = self.recv_socket.recv(48)
+        data_decoded = struct.unpack('ffffffffffff', data)
 
-        # Print the received data
-        print(f"Received data: {data}")
-
-    def send_hello(self):
-        try:
-            # send a message to the ESP32
-            message = 'Hello, ESP32!'
-            self.send_socket.send(message.encode())
-
-            # Wait for a response from the ESP32
-            response = self.send_socket.recv(1024).decode()
-            
-            if response == "200":
-                print("Received response from ESP32:", response)
-            else:
-                print("Invalid response from ESP32, reconnecting...")
-                self.connect()
-                self.send_hello()
-
-        except Exception as e:
-            print("Connection error :", e)
-            self.connect()    
+        if data:
+            self.recv_socket.send("200".encode())
+            self.recv_stat.append([1, time.time()-self.time])
+            # Print the received data
+            print(f"Received data: {data_decoded}")
 
 
-    def send_angles(self, angle1, angle2, angle3):
-        # Encode the angles as floats and pack them into a binary format
-        packed_angles = struct.pack('fff', angle1, angle2, angle3)
-
-    
+    def _send_hello(self):
         try:
             # Send the packed angles over the socket
-            self.client.sendall(packed_angles)
+            packed_angles = struct.pack('fff', 2, 3, 4)
+            self.send_socket.sendall(packed_angles)
+            print("Send Message")
 
             # Wait for a response from the ESP32
-            response = self.client.recv(1024).decode()
-            
-            if response == "200":
+            response = 0
+
+            while (_is_socket_alive(self) and not response == "200") :
+                response = self.send_socket.recv(1024).decode()   
                 print("Received response from ESP32:", response)
-            else:
+
+            if not _is_socket_alive(self) :  
                 print("Invalid response from ESP32, reconnecting...")
-                self.connect()
-                self.send_angles(angle1,angle2,angle3)
+                self.recv_socket.close()
+                self.send_socket.close()
+                self._connect() 
+                self._send_hello()
+
+                
 
         except Exception as e:
             print("Connection error :", e)
-            self.connect()
+            self.connected = False
 
-    def setting(self, speed1, speed2):
-        # Encode the angles as floats and pack them into a binary format
-        packed_setting = struct.pack('fff', speed1, speed2)
+############ HELPER METHOD ########
 
-        try:
-            # Send the packed angles over the socket
-            self.client.sendall(packed_setting)
-
-            # Wait for a response from the ESP32
-            response = self.client.recv(1024).decode()
-
-            if response == "200":
-                print("Received response from ESP32:", response)
-      
-            else:
-                print("Invalid response from ESP32, reconnecting...")
-                self.connect()
-                self.setting(self,speed1,speed2)
-
-        except Exception as e:
-            print("Connection error :", e)
-            self.connect()
+def _is_socket_alive(self):
+        err = self.send_socket.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+        return err == 0
