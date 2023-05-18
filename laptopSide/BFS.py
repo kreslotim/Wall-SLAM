004ddbdb570d451f45a6f0b472e1a926f97fa556
Finn MacNamara <131534823+finn-macnamara@users.noreply.github.com>
2023-05-18 19:17:52 +0200

    BFS pathfinding

---
+++ laptopSide/BFS.py
@@ -0,0 +1,87 @@
+from collections import deque
+
+# example positions nfor tests
+current_position = (0, 0)
+destination = (0, 19)
+
+# example grid to test, representing the explorable map with 0=free and 1=obstacle
+grid = [
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+# We need access to the current position
+def shortest_path(current_position, destination, grid):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    # Check for trivial case: the current position is the destination
+    if current_position == destination:
+        return []
+
+    # Create a queue for BFS algo (decided against Dijkstra) and initialize with  current position
+    queue = deque([(current_position, [])])
+
+    # Create a set to track visited positions
+    visited = set([current_position])
+
+    # Define possible directions: up, down, left, right
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    direction_names = ['up', 'down', 'left', 'right']
+
+    while queue:
+        current_pos, path = queue.popleft()
+
+        # Check if the current position is the destination
+        if current_pos == destination:
+            return path
+
+        x, y = current_pos
+
+        # Explore all possible directions
+        for i in range(4):
+            dx, dy = directions[i]
+            new_x = x + dx
+            new_y = y + dy
+
+            # Check if the new position is within the grid boundaries
+            if 0 <= new_x < rows and 0 <= new_y < cols:
+                new_pos = (new_x, new_y)
+
+                # Check if the new position is not an obstacle and not visited
+                if grid[new_x][new_y] == 0 and new_pos not in visited:
+                    queue.append((new_pos, path + [direction_names[i]]))
+                    visited.add(new_pos)
+
+    # If there is no path to the destination
+    return None
+
+
+
+
+path = shortest_path(current_position, destination, grid)
+if path is None:
+    print("There is no path to the destination.")
+else:
+    print("Shortest path:", path)
+
+    # How can we turn global direction instructions into POV instructions?
