bf027bc1fdef1f0a35e1a75105b6b61e64457bb5
FacerOfGod <spichot2@gmail.com>
2023-06-01 20:25:13 +0200

    routes.py and run.py documented

--- web/app/routes.py
+++ web/app/routes.py
@@ -4,7 +4,6 @@ import time
 import json
 
 from app.models.esp32 import ESP32Connection
-
 from app.models.mapK import ClusterChart
 
 main = Blueprint('main', __name__)
@@ -13,38 +12,39 @@ ip="168.20.13.1"
 recv_port = 8888
 send_port = 8889
 
-# Initial robot position and obstacle data
-
-global togo
-togoX = (0,0)
-
-startTime= time.time()
-
 espT = ESP32Connection(send_port=send_port,recv_port=recv_port)
-#espT.start_thread() # Always on
+startTime = time.time()
+
+# Initial robot position and obstacle data
+global togo
+togo = (0,0)
+
+# espT.start_thread() # Always on
 global cluster_chart
 cluster_chart  =  ClusterChart()
 
+# Know if you are connected or not
 global settingConnection
 settingConnection = False
 
-global settingChewbaccaAuto
-settingChewbaccaAuto = False
-
-global settingDataESP
-settingDataESP = False
-
-global settingDataSIM
-settingDataSIM = False
-
 @main.route('/')
 def index():
+    """
+    Route handler for the root URL ("/").
+    Renders the "index.html" template and passes the 'ip' variable to it.
+    """
     return render_template('index.html', ip=ip)
 
 
 ############ MOVEMENT API ############
 @main.route('/post-move', methods=['POST'])
 def post_move():
+    """
+    Route handler for the "/post-move" endpoint with the POST method.
+    Receives the 'direction' parameter from the request form and sends
+    corresponding commands to the ESP32 based on the direction.
+    Returns a JSON response indicating the command status.
+    """
     direction = request.form.get('direction')
     repCode = 0
     print("moving")
@@ -75,6 +75,10 @@ def post_move():
 ############ GRAPH API ############ 
 @main.route('/get-graph-distance', methods=['GET'])
 def get_graph_distance():
+    """
+    Route handler for the "/get-graph-distance" endpoint with the GET method.
+    Retrieves the orientation from espT.slam_data and returns it as a JSON response.
+    """
     list_of_obs = espT.slam_data.list_of_temp_orr
 
     # Create multiple arrays
@@ -91,17 +95,20 @@ def get_graph_distance():
         'time': result[3]
     }
 
-
     return jsonify(data=json.dumps(response_data))
 
 
 @main.route('/get-graph-com', methods=['GET'])
 def get_graph_com():
+    """
+    Route handler for the "/get-graph-com" endpoint with the GET method.
+    Retrieves the number of data packages transfer from espT and returns it as a JSON response.
+    """
     print(" COM ")
     if espT.connected:
             # Retrieve the time data for 'Sent' and 'Received' traces
         sent_data = espT.send_stat
-        received_data =espT.recv_stat
+        received_data = espT.recv_stat
         obs_data= espT.obs_stat
         
         # Create a dictionary with the time data
@@ -118,6 +125,10 @@ def get_graph_com():
 
 @main.route('/get-graph-obs-raw', methods=['GET'])
 def get_graph_obs_raw():
+    """
+    Route handler for the "/get-graph-obs-raw" endpoint with the GET method.
+    Retrieves the raw list of obstacles with from espT.slam_data and returns it as a JSON response.
+    """
     print(" obs-raw ")
     response_data = {
         'x_car': espT.slam_data.curr_x_car,
@@ -129,6 +140,10 @@ def get_graph_obs_raw():
 
 @main.route('/get-graph-redundancy', methods=['GET'])
 def get_graph_redundancy():
+    """
+    Route handler for the "/get-graph-redundancy" endpoint with the GET method.
+    Retrieves filter list of data from espT.slam_data and returns it as a JSON response.
+    """
     list_of_obs = espT.slam_data.filter_obstacles()
     x_obs = [coord[0] for coord in list_of_obs]
     y_obs = [coord[1] for coord in list_of_obs]
@@ -142,10 +157,14 @@ def get_graph_redundancy():
     return jsonify(data=json.dumps(response_data))
 @main.route('/get-graph-kmeans', methods=['GET'])
 def get_graph_kmeans():
+    """
+    Route handler for the "/get-graph-kmeans" endpoint with the GET method.
+    Generates a chart JSON using cluster_chart with K-means and returns it as a JSON response.
+    """
     global cluster_chart, togo
     mapJson, togo, rectangle = cluster_chart.generate_chart_json(espT.slam_data.list_of_obs)
 
-  # Generate the list of occupied cells
+    # Generate the list of occupied cells
     cells = []
     for rec in rectangle:
         print(rec)
@@ -164,26 +183,35 @@ def get_graph_kmeans():
 
 @main.route('/get-graph-movement', methods=['POST'])
 def get_graph_movement():
-        if 'togo' in request.form:
-            togo_coordinates = json.loads(request.form['togo'])
-            espT.path_finder.setTarget_xy_in_website( togo_coordinates)
-            espT.map_all()
+    """
+    Route handler for the "/get-graph-movement" endpoint with the POST method.
+    Receives targeted coordinates from the mouse-click on the map, sets the target in espT.path_finder and forces espT.path_finder to run Dijkstra, and returns a JSON response with grid data, path coordinates, and angle.
+    """
+    if 'togo' in request.form:
+        togo_coordinates = json.loads(request.form['togo'])
+        espT.path_finder.setTarget_xy_in_website( togo_coordinates)
+        espT.map_all()
             
-        x_route = [coord[0] for coord in espT.path_finder.path]
-        y_route = [coord[1] for coord in espT.path_finder.path]
+    x_route = [coord[0] for coord in espT.path_finder.path]
+    y_route = [coord[1] for coord in espT.path_finder.path]
 
-        response_data = {   
-        'gridData': espT.path_finder.generate_list_of_obstacles_for_website(),  
-        'pathX': x_route,
-        'pathY': y_route,
-        'angle' : espT.slam_data.list_of_temp_orr[-1] if len(espT.slam_data.list_of_temp_orr) != 0 else []
-        }
+    response_data = {   
+    'gridData': espT.path_finder.generate_list_of_obstacles_for_website(),  
+    'pathX': x_route,
+    'pathY': y_route,
+    'angle' : espT.slam_data.list_of_temp_orr[-1] if len(espT.slam_data.list_of_temp_orr) != 0 else []
+    }
 
-        return jsonify(data=json.dumps(response_data))
+    return jsonify(data=json.dumps(response_data))
 
 ############ SETTING API ############
 @main.route('/update_kmean_slider', methods=['POST'])
 def update_kmean_slider():
+    """
+    Route handler for the "/update_kmean_slider" endpoint with the POST method.
+    Updates the values of chart based on the received slider values.
+    Returns a success message.
+    """
     global cluster_chart
     filter_value = int(request.form.get('filter'))
     max_k_value = int(request.form.get('max_k'))
@@ -195,20 +223,22 @@ def update_kmean_slider():
 
 @main.route('/get-status-value', methods=['GET'])
 def get_status_value():
+    """
+    Route handler for the "/get-status-value" endpoint with the GET method.
+    Returns a JSON response with the status values including the connection status,
+    ESP32 status, host IP, and host name.
+    """
     return jsonify({'setting' : settingConnection, 'status': espT.connected, 'hostIP' : espT.hostIp, 'hostName' : espT.hostName})
 
 @main.route('/update-switch-state-setting', methods=['POST'])
 def update_switch_state_data():
-    global settingConnection, settingChewbaccaAuto, settingDataESP, settingDataSIM
+    """
+    Route handler for the "/update-switch-state-setting" endpoint with the POST method. Updates the switch state value and starts or stops the ESP32 thread accordingly. Returns a JSON response indicating the success status.
+    """
+    global settingConnection
 
     sSettingConnection = request.form.get('connection')
-    sSettingChewbaccaAuto = request.form.get('auto')
-    sSettingDataESP = request.form.get('dataESP')
-    sSettingDataSIM = request.form.get('dataSIM')
 
-    settingChewbaccaAuto = (sSettingChewbaccaAuto == "true")
-    settingDataESP = (sSettingDataESP == "true")
-    settingDataSIM = (sSettingDataSIM == "true")
     print("updating")
     if not settingConnection and sSettingConnection == "true":
         espT.start_thread()
--- web/run.py
+++ web/run.py
@@ -2,4 +2,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    """
+    Run the Flask Application
+    """
     app.run()
